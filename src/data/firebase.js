b70534bd409503399ff67ffe9ff2870802ffbde6
JPsonicACE <jp.filiatrault@gmail.com>
2021-03-16 20:00:23 -0400

    fini

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,7 +4,12 @@ import 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyBv44JPPQoImY8w0DDBFSlvEraIE58oQgY",
+  authDomain: "ex3-produits-7e626.firebaseapp.com",
+  projectId: "ex3-produits-7e626",
+  storageBucket: "ex3-produits-7e626.appspot.com",
+  messagingSenderId: "343154393616",
+  appId: "1:343154393616:web:fb4cc05002f29c6dd18095"
 };
 
 // Initialiser Firebase
@@ -16,3 +21,4 @@ if(!fbAppli.apps.length) {
 const bd = fbAppli.firestore();
 /******* Ex#3 - Étape C ********************************/ 
 // Exporter (par défaut) la référence à "bd" pour pouvoir l'utiliser dans le composant ListeProduits
+export default bd;
